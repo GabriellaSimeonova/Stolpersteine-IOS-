10ad3e935c07bb5660d96a95752cdffcc2d740c7
Radolina <radolina@Radolinas-MBP.home>
2023-06-06 12:23:01 +0200

    Initialization of Project

---
+++ Stolpersteine/Stolpersteine/StolpersteineApp.swift
@@ -0,0 +1,17 @@
+//
+//  StolpersteineApp.swift
+//  Stolpersteine
+//
+//  Created by Radolina on 06/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct StolpersteineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
