5effc1b664224d82f83718ef42a12caf31847f21
Gabriela Simeonova <477569@student.fontys.nl>
2023-06-07 19:22:53 +0200

    #1 candle lightning function done TODO: Add text into the component

--- Stolpersteine/Stolpersteine/Components/CandleLightningView.swift
+++ Stolpersteine/Stolpersteine/Components/CandleLightningView.swift
@@ -8,17 +8,32 @@
 import SwiftUI
 
 struct CandleLightningView: View {
+    @State private var imageName = "CandleDark"
+    @State private var isCandleLit = false // New state variable
+
     var body: some View {
         ZStack {
-            
-            Image("CandleDark")
+            Image(imageName) // Use the dynamic image name
                 .resizable()
                 .scaledToFill()
                 .clipShape(RoundedRectangle(cornerRadius: 15))
                 .frame(width: UIScreen.main.bounds.width * 0.9, height: 200)
+
+            VStack {
+                Text("Name").padding().foregroundColor(.red)
+
+                if !isCandleLit {
+                    LightTheCandleButtonView(isCandleLit: $isCandleLit) // Pass the binding to the button
+                }
+            }
+        }
+        .onChange(of: isCandleLit) { newValue in
+            imageName = newValue ? "CandleLit" : "CandleDark" // Update image name when isCandleLit changes
         }
     }
 }
+
+
 
 struct CandleLightningView_Previews: PreviewProvider {
     static var previews: some View {
--- Stolpersteine/Stolpersteine/Components/LightTheCandleButtonView.swift
+++ Stolpersteine/Stolpersteine/Components/LightTheCandleButtonView.swift
@@ -7,36 +7,42 @@
 
 import SwiftUI
 
+
 struct LightTheCandleButtonView: View {
-    var body: some View {
-        Button(action: {
-            // Button action here
+    @Binding var isCandleLit: Bool // Use a binding for the state variable
+       
+       var body: some View {
+           Button(action: {
+               isCandleLit.toggle() 
         }) {
-            VStack {
+            VStack (spacing: 5){
                 Text("Light")
                     .font(.headline)
                     .foregroundColor(Color(hex: "7F462C"))
-                Text("the candle")
+                Text("candle")
                     .font(.headline)
                     .foregroundColor(Color(hex: "7F462C"))
                 
                 Image(systemName: "flame.fill")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 40, height: 40)
+                    .frame(width: 30, height: 30)
                     .foregroundColor(Color(hex: "7F462C"))
              
             }
             .padding()
             .background(Color(hex: "F1D0B5"))
             .clipShape(Circle())
+            .frame(width: 110, height: 110)
             .shadow(radius: 3)
         }
     }
 }
 
 struct LightTheCandleButtonView_Previews: PreviewProvider {
+    @State static var isCandleLit = false // Create a state variable for preview
+    
     static var previews: some View {
-        LightTheCandleButtonView()
+        LightTheCandleButtonView(isCandleLit: $isCandleLit) // Provide a binding for isCandleLit
     }
 }
